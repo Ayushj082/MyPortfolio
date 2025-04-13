96f48c42f2b9e7f9ad069ab7af47aca777cc555e
Ayushman Jaiswal <ayushmanj082@gmail.com>
2025-04-14 00:54:56 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Hero from "./sections/hero"
+const App =()=>{
+    return (
+        <main>
+            <div>
+                <Hero />
+            </div>
+        </main>
+    )
+}
+export default App
